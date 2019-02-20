4430f4094829dc53f1545ca8886efeeba7cbf6d0
masahiro <mkiji@coe.edu>
2019-02-20 11:38:12 -0600

    Add Go class

---
+++ src/Go.java
@@ -0,0 +1,19 @@
+public class Go extends MonopolySquare {
+
+    public Go(String name){
+
+        super(name);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public void landOn(Player P) {
+
+
+    }
+
+
+}
